70be202f99d1bb8a0af5aac788265b8afc4c7530
Jack Jamison <jackqjamison@gmail.com>
2026-01-25 19:24:42 -0500

    da_init

--- src/da.h
+++ src/da.h
@@ -6,15 +6,21 @@
 #define DA(type) struct {                       \
         size_t count;                           \
         size_t capacity;                        \
-        type items;                             \
+        type *items;                            \
     }
 
 #define DA_DEFINE(type, name) typedef DA(type) name
 
+#define arena_da_create(a, da, size) do {                               \
+        (da)->count = size;                                             \
+        (da)->capacity = size;                                          \
+        (da)->items = arena_alloc((a), (da)->capacity*sizeof(*((da)->items)), _Alignof(*((da)->items))); \
+    } while (0)
+
 #define arena_da_append(a, da, item) do {                               \
         if ((da)->count >= (da)->capacity) {							\
             usize new_capacity = (da)->capacity == 0 ? ARENA_DA_INIT_CAP : (da)->capacity*2; \
-            (da)->items = arena_realloc((a), (da)->items, (da)->capacity*sizeof(*((da)->items)), new_capacity*sizeof(*((da)->items))); \
+            (da)->items = arena_realloc((a), (da)->items, (da)->capacity*sizeof(*((da)->items)), new_capacity*sizeof(*((da)->items)), _Alignof(*((da)->items))); \
             (da)->capacity = new_capacity;								\
         }																\
                                                                         \
@@ -27,11 +33,21 @@
             usize new_capacity = (da)->capacity;                        \
             if (new_capacity == 0) new_capacity = ARENA_DA_INIT_CAP;    \
             while ((da)->count + (new_items_count) > new_capacity) new_capacity *= 2; \
-            (da)->items = arena_realloc((a), (da)->items, (da)->capacity*sizeof(*((da)->items)), new_capacity*sizeof(*(da)->items)); \
+            (da)->items = arena_realloc((a), (da)->items, (da)->capacity*sizeof(*((da)->items)), new_capacity*sizeof(*(da)->items), _Alignof(*((da)->items))); \
             (da)->capacity = new_capacity;                              \
         }                                                               \
         memcpy((da)->items + (da)->count, (new_items), (new_items_count)*sizeof(*((da)->items))); \
         (da)->count += (new_items_count);                               \
+    } while (0)
+
+#define da_create(da, size) do {                                        \
+        (da)->count = size;                                             \
+        (da)->capacity = size;                                          \
+        (da)->items = malloc((da)->capacity*sizeof(*((da)->items)));\
+    } while (0)
+
+#define da_destroy(da) do {                                             \
+        free((da)->items);                                              \
     } while (0)
 
 #define da_append(da, item) do {                                        \
